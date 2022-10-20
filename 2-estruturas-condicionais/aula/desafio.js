e5eff1825cf28ca1c9ede63ac203351bba5f9cd3
Camila Zucchi <camilazucchi@outlook.com>
2022-10-20 18:16:43 -0300

    desafio estruturas condicionais

---
+++ 2-estruturas-condicionais/aula/desafio.js
@@ -0,0 +1,28 @@
+/*
+faça um programa para calcular o valor de uma viagem.
+
+você terá cinco variáveis, sendo elas:
+    1 - preço do etanol;
+    2 - preço da gasolina;
+    3 - o tipo de combustível que está no seu carro;
+    4 - o gasto médio de combustível do carro por km;
+    5 - distância em km da viagem.
+
+imprima no console o valor que será  gasto para realizar esta viagem.
+*/
+
+const precoEtanol = 3.40;
+const precoGasolina = 4.86;
+const gastoMedioPorKm = 12;
+const distanciaEmKm = 180.4;
+const isEtanol = false;
+
+if (isEtanol === true) {
+    const litrosConsumidos = distanciaEmKm / gastoMedioPorKm;
+    const valorGasto = litrosConsumidos * precoEtanol;
+    console.log("você gastará: " + "R$" +valorGasto.toFixed(2) + " em combustível do tipo etanol. boa viagem!");
+} else {
+    const litrosConsumidos = distanciaEmKm / gastoMedioPorKm;
+    const valorGasto = litrosConsumidos * precoGasolina;
+    console.log("você gastará: " + "R$" +valorGasto.toFixed(2) + " em combustível do tipo gasolina. boa viagem!");
+}
