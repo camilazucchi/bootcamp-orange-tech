0d48133f683a66ddcd575d2db1941f7fb4131ccb
Camila Zucchi <camilazucchi@outlook.com>
2022-10-25 14:05:41 -0300

    organizando pastas

---
+++ 4-objetos/aula.js
@@ -0,0 +1,10 @@
+// síntaxe de um objeto:
+const cami = {
+    // chave e valor;
+    nome: 'camila zucchi',
+    idade: 25
+};
+
+console.log(cami.nome);
+console.log(cami.idade);
+console.log(cami);
